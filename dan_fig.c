8430b580a499f0ddb747a0a1363c8ce91d7b2696
riley potter <riley.potter12@gmail.comgit config --global user.email riley.potter12@gmail.comgit config --global user.email riley.potter12@gmail.com>
2014-10-16 17:47:15 -0500

    Added random function
    made changes to main function and also added a function that will print
    two different outputs.

--- dan_fig.c
+++ dan_fig.c
@@ -1,6 +1,32 @@
 #include <stdio.h>
+#include <time.h>
+#include <stdlid.h>
 
 int main(void)
 {
+	srand=tim(NULL);
+	
+	int x=0;
+	int y=0;
+
+	printf("\nPlease enter some number: ");
+	scanf("%d", &y);
+	x = random(y);
+	
+	if(x >0 || x < 6)
+	{
+		printf("\nI love dan!");
+	}
+	else
+	{
+		printf("\nDan is the man!")
+	}
 	return 0;
+}
+
+int random(int y)
+{
+	int z=0;
+	z = y % random()%10;
+	return z;
 }
